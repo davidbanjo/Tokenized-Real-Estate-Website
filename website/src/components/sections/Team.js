d3acd606d5e5c6975ca79b4f8a338095652defbf
davidbanjo <davidbanjo879@gmail.com>
2025-01-03 14:54:21 +0100

    team members removed

--- website/src/components/sections/Team.js
+++ website/src/components/sections/Team.js
@@ -32,29 +32,29 @@ const Team = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-					<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">
-						<div className="profile-card_image">
-							<img src="img/team/dean.jpg" alt="User" className="mb-4 shadow" />
-						</div>
-						<div className="profile-card_details">
-							<h3 className="mb-0">Dean Enzo</h3>
-							<p className="text-muted">COO</p>
-							<p className="text-muted">A goal driven individual with experience in managing various entreprenueral project based activities across West-Africa</p>
-						</div>
-						<div className="profile-card_social text-center p-4">
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/linkedin.png" alt="Linkedin" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/twitter.png" alt="Twitter" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/facebook.png" alt="Facebook" />
-							</a>
-						</div>
-					</div>
-				</div>
+				{/*<div className="col-lg-4 col-md-6 col-sm-12 mb-4">*/}
+				{/*	<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">*/}
+				{/*		<div className="profile-card_image">*/}
+				{/*			<img src="img/team/dean.jpg" alt="User" className="mb-4 shadow" />*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_details">*/}
+				{/*			<h3 className="mb-0">Dean Enzo</h3>*/}
+				{/*			<p className="text-muted">COO</p>*/}
+				{/*			<p className="text-muted">A goal driven individual with experience in managing various entreprenueral project based activities across West-Africa</p>*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_social text-center p-4">*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/linkedin.png" alt="Linkedin" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/twitter.png" alt="Twitter" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/facebook.png" alt="Facebook" />*/}
+				{/*			</a>*/}
+				{/*		</div>*/}
+				{/*	</div>*/}
+				{/*</div>*/}
 				<div className="col-lg-4 col-md-6 col-sm-12 mb-4">
 					<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">
 						<div className="profile-card_image">
@@ -78,52 +78,52 @@ const Team = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-					<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">
-						<div className="profile-card_image">
-							<img src="img/team/nelo.jpg" alt="User" className="mb-4 shadow" />
-						</div>
-						<div className="profile-card_details">
-							<h3 className="mb-0">Chinelonma Anaetoh</h3>
-							<p className="text-muted">CHRO</p>
-							<p className="text-muted">An experienced HR and business consultant passionate creating wonderful human experiences for organizations, employees and its customers. </p>
-						</div>
-						<div className="profile-card_social text-center p-4">
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/linkedin.png" alt="Linkedin" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/twitter.png" alt="Twitter" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/facebook.png" alt="Facebook" />
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-					<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">
-						<div className="profile-card_image">
-							<img src="img/team/david.jpg" alt="User" className="mb-4 shadow" />
-						</div>
-						<div className="profile-card_details">
-							<h3 className="mb-0">David Banjo</h3>
-							<p className="text-muted">Operations Manager</p>
-							<p className="text-muted">An experienced web designer and team lead with a proven track record of managing teams of different sizes</p>
-						</div>
-						<div className="profile-card_social text-center p-4">
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/linkedin.png" alt="Linkedin" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/twitter.png" alt="Twitter" />
-							</a>
-							<a href="#!" className="d-inline-block">
-								<img src="img/social/facebook.png" alt="Facebook" />
-							</a>
-						</div>
-					</div>
-				</div>
+				{/*<div className="col-lg-4 col-md-6 col-sm-12 mb-4">*/}
+				{/*	<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">*/}
+				{/*		<div className="profile-card_image">*/}
+				{/*			<img src="img/team/nelo.jpg" alt="User" className="mb-4 shadow" />*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_details">*/}
+				{/*			<h3 className="mb-0">Chinelonma Anaetoh</h3>*/}
+				{/*			<p className="text-muted">CHRO</p>*/}
+				{/*			<p className="text-muted">An experienced HR and business consultant passionate creating wonderful human experiences for organizations, employees and its customers. </p>*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_social text-center p-4">*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/linkedin.png" alt="Linkedin" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/twitter.png" alt="Twitter" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/facebook.png" alt="Facebook" />*/}
+				{/*			</a>*/}
+				{/*		</div>*/}
+				{/*	</div>*/}
+				{/*</div>*/}
+				{/*<div className="col-lg-4 col-md-6 col-sm-12 mb-4">*/}
+				{/*	<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">*/}
+				{/*		<div className="profile-card_image">*/}
+				{/*			<img src="img/team/david.jpg" alt="User" className="mb-4 shadow" />*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_details">*/}
+				{/*			<h3 className="mb-0">David Banjo</h3>*/}
+				{/*			<p className="text-muted">Operations Manager</p>*/}
+				{/*			<p className="text-muted">An experienced web designer and team lead with a proven track record of managing teams of different sizes</p>*/}
+				{/*		</div>*/}
+				{/*		<div className="profile-card_social text-center p-4">*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/linkedin.png" alt="Linkedin" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/twitter.png" alt="Twitter" />*/}
+				{/*			</a>*/}
+				{/*			<a href="#!" className="d-inline-block">*/}
+				{/*				<img src="img/social/facebook.png" alt="Facebook" />*/}
+				{/*			</a>*/}
+				{/*		</div>*/}
+				{/*	</div>*/}
+				{/*</div>*/}
 				<div className="col-lg-4 col-md-6 col-sm-12 mb-4">
 					<div className="profile-card bg-white shadow mb-4 text-center rounded-lg p-4 position-relative h-100">
 						<div className="profile-card_image">
